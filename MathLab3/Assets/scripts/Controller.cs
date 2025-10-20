3374a485da02dad907b5cc71ce4f447aaaab1e67
Oooga617 <johnphilip.underwood@ontariotechu.net>
2025-10-19 20:50:55 -0400

    sus

--- MathLab3/Assets/scripts/Controller.cs
+++ MathLab3/Assets/scripts/Controller.cs
@@ -8,6 +8,8 @@ public class Controller : MonoBehaviour
     public GameObject Target;
     private NavMeshAgent agent;
     private Animator animator;
+    float characterSpeedSlider;
+
 
     // Start is called before the first frame update
     void Start()
@@ -40,5 +42,24 @@ public class Controller : MonoBehaviour
             animator.SetBool("Attack", false);
             animator.SetBool("Walk", true);
         }
+    }
+
+    void OnGUI()
+    {
+        float additionalSpeed = 1.0f;
+
+        //Create a Label in Game view for the Slider
+        GUI.Label(new Rect(0, 25, 40, 60), "Knight Speed");
+
+        //Create a horizontal Slider to control the speed of the Animator. Drag the slider to 1 for normal speed.
+
+        characterSpeedSlider = GUI.HorizontalSlider(new Rect(45, 25, 200, 60), characterSpeedSlider, 0.0F, 5.0F);
+      
+        //Make the speed of the Animator match the Slider value
+        animator.speed = characterSpeedSlider;
+        agent.speed = animator.speed * 2f;
+        
+        
+
     }
 }
